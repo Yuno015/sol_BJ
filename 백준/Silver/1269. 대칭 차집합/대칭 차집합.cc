47fe7ef2883968104aeeac777e72de9b91000ef8
jjjoong <131260594+jjjoong@users.noreply.github.com>
2023-05-11 21:04:56 +0900

    [Silver IV] Title: 대칭 차집합, Time: 244 ms, Memory: 31044 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<string> noH;
+	vector<string> noS;
+
+	while (N--)
+	{
+		string temp;
+		cin >> temp;
+		noH.push_back(temp);
+	}
+	while (M--)
+	{
+		string temp;
+		cin >> temp;
+		noS.push_back(temp);
+	}
+	sort(noH.begin(), noH.end());
+	sort(noS.begin(), noS.end());
+
+	vector<string> ret;
+	ret.resize(noH.size() + noS.size());
+	auto it = set_symmetric_difference(noH.begin(), noH.end(), noS.begin(), noS.end(), ret.begin());
+	ret.erase(it, ret.end());
+	sort(ret.begin(), ret.end());
+
+	cout << ret.size() << "\n";
+	
+	return 0;
+}
