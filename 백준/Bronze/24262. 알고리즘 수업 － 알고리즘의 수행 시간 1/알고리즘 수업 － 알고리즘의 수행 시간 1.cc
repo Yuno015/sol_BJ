b30187c840e0131a58f9bcce1f2165eccdad1a42
jjjoong <131260594+jjjoong@users.noreply.github.com>
2023-05-19 18:26:56 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int N;
+	cin >> N;
+
+	cout << 1 << "\n";
+	cout << 0 << "\n";
+	return 0;
+}
