91aba91a15faab24386591d365642d80d373c5c2
jjjoong <131260594+jjjoong@users.noreply.github.com>
2023-05-22 19:51:40 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int r1, s;
+	cin >> r1 >> s;
+
+	int r2 = 2 * s - r1;
+
+	cout << r2 << "\n";
+
+	
+	return 0;
+}
