666da9d28460b996bcc215e51143ab7ef1f3e4a9
jjjoong <131260594+jjjoong@users.noreply.github.com>
2023-06-04 23:38:57 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	long long N, M;
+	cin >> N >> M;
+
+	cout << abs(N - M) << "\n";
+	return 0; 
+}
