30c4e29e946d00aef4ad2a5dbc9685a2a2ad2bb8
jjjoong <131260594+jjjoong@users.noreply.github.com>
2023-06-08 21:42:49 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int A, I;
+	cin >> A >> I;
+
+	cout << A * (I - 1) + 1 << "\n";
+	return 0;
+}
