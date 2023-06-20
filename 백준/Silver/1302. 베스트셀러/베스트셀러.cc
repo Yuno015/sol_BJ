ea67ba0e031145d6bdfe3f9eb9253854e32bf663
jjjoong <131260594+jjjoong@users.noreply.github.com>
2023-06-20 22:20:49 +0900

    [Silver IV] Title: 베스트셀러, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.cc
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool cmp(pair<string, int>& a, pair<string, int>& b)
+{
+	if (a.second == b.second)
+	{
+		return a.first < b.first;
+	}
+	else
+		return a.second > b.second;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int N;
+	cin >> N;
+
+	unordered_map<string, int> mp;
+	while (N--)
+	{
+		string temp;
+		cin >> temp;
+
+		auto iter = mp.find(temp);
+		if (iter != mp.end())
+		{
+			iter->second++;
+		}
+		else
+		{
+			mp.insert(make_pair(temp, 1));
+		}
+	}
+
+	vector<pair<string, int>> arr(mp.begin(), mp.end());
+	sort(arr.begin(), arr.end(), cmp);
+
+	cout << arr[0].first << "\n";
+	return 0;
+}
