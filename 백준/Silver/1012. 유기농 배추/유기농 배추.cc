c904cb9b07a2d081343e7103d84c302d30f83bd3
jjjoong <131260594+jjjoong@users.noreply.github.com>
2023-08-04 16:48:13 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int n = 51;
+int arr[n][n];
+int visited[n][n];
+int cnt = 0;
+
+int T;
+int N, M, K;
+
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
+
+void DFS(int y, int x)
+{
+	visited[y][x] = cnt;
+
+	for (int i = 0; i < 4; i++)
+	{
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+
+		if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+		if (visited[ny][nx] == 0 && arr[ny][nx] == 1)
+		{
+			DFS(ny, nx);
+		}
+	}
+	return;
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+
+	while (T--)
+	{
+		fill(&arr[0][0], &arr[0][0] + 51 * 51, 0);
+		fill(&visited[0][0], &visited[0][0] + 51 * 51, 0);
+		cnt = 0;
+		cin >> M >> N >> K;
+
+		for (int i = 0; i < K; i++)
+		{
+			int x, y;
+			cin >> x >> y;
+			arr[y][x] = 1;
+		}
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < M; j++)
+			{
+				if (arr[i][j] == 1 && visited[i][j] == 0)
+				{	
+					cnt++;
+					DFS(i, j);
+				}
+			}
+		}
+
+		cout << cnt << "\n";
+
+		
+	}
+	return 0;
+}
