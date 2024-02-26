783299412cf480a9c80652ef953b42083e998bcf
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-02-26 13:20:04 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -2,58 +2,45 @@
 
 using namespace std;
 
-const int n = 105;
-int arr[n][n];
-int visited[n][n];
-int N, M;
+int arr[104][104];
+int visited[104][104];
 
 const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, 1, 0, -1 };
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
+	int N, M;
 	cin >> N >> M;
-	cin.ignore();
 
-	for (int i = 0; i < N; i++)
+	for (int i = 1; i <= N; i++)
 	{
-		string temp;
-		getline(cin, temp);
-
-		for (int j = 0; j < temp.size(); j++)
+		for (int j = 1; j <= M; j++)
 		{
-			arr[i][j] = temp[j] - '0';
+			scanf("%1d", &arr[i][j]);
 		}
 	}
 
 	queue<pair<int, int>> q;
-	q.push(make_pair(0, 0));
-	visited[0][0] = 1;
+	q.push({ 1, 1 });
+	visited[1][1] = 1;
+	
 	while (q.size())
 	{
-		int hereY = q.front().first;
-		int hereX = q.front().second;
-
-		q.pop();
-
+		int x, y, nx, ny;
+		tie(y, x) = q.front(); q.pop();
 		for (int i = 0; i < 4; i++)
 		{
-			int ny = hereY + dy[i];
-			int nx = hereX + dx[i];
+			ny = y + dy[i];
+			nx = x + dx[i];
 
-			if (ny < 0 || ny >= N || nx < 0 || nx >= M || arr[ny][nx] == 0) continue;
-			if (visited[ny][nx]) continue;
-
-			visited[ny][nx] = visited[hereY][hereX] + 1;
-			q.push(make_pair(ny, nx));
+			if (ny < 1 || nx < 1 || ny > N || nx > M) continue;
+			if (visited[ny][nx] != 0 || arr[ny][nx] == 0) continue;
+			q.push({ ny, nx });
+			visited[ny][nx] = visited[y][x] + 1;
 		}
 	}
 
-	cout << visited[N-1][M-1] << "\n";
-
+	cout << visited[N][M] << "\n";
 	return 0;
 }
