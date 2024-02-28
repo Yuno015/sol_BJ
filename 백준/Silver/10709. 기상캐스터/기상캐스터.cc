a29ebde470bd00ad40c34d7b6a1adeffed812a1f
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-02-28 10:49:56 +0900

    [Silver V] Title: 기상캐스터, Time: 4 ms, Memory: 2124 KB -BaekjoonHub

--- 백준/Silver/10709. 기상캐스터/기상캐스터.cc
+++ 백준/Silver/10709. 기상캐스터/기상캐스터.cc
@@ -2,55 +2,53 @@
 
 using namespace std;
 
-const int n = 101;
-char arr[n][n];
-int result[n][n];
+char arr[104][204];
+int ret[104][204];
 
 int main(void)
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-
 	int H, W;
 	cin >> H >> W;
+
+	int cnt = 1;
+
+	memset(ret, -1, sizeof(ret));
 
 	for (int i = 0; i < H; i++)
 	{
 		for (int j = 0; j < W; j++)
 		{
 			cin >> arr[i][j];
+			if (arr[i][j] == 'c') ret[i][j] = 0;
 		}
 	}
 
-	for (int i = 0; i < H; i++)
+	for (int n = 0; n < W; n++)
 	{
-		if (arr[i][0] == 'c')
-			result[i][0] = 0;
-		else if (arr[i][0] == '.')
-			result[i][0] = -1;
-
-		for (int j = 1; j < W; j++)
+		for (int i = 0; i < H; i++)
 		{
-			if (arr[i][j] == 'c')
-				result[i][j] = 0;
-			else
+			for (int j = W - 1; j > 0; j--)
 			{
-				result[i][j] = -1;
-				for (int k = j - 1; k >= 0; k--)
+				arr[i][j] = arr[i][j - 1];
+			}
+			arr[i][0] = '.';
+
+			for (int j = 0; j < W; j++)
+			{
+				if (arr[i][j] == 'c' && ret[i][j] == -1)
 				{
-					if (arr[i][k] == 'c' && result[i][j] == -1)
-						result[i][j] = (j - k);
+					ret[i][j] = cnt;
 				}
 			}
 		}
+		cnt++;
 	}
 
 	for (int i = 0; i < H; i++)
 	{
 		for (int j = 0; j < W; j++)
 		{
-			cout << result[i][j] << " ";
+			cout << ret[i][j] << " ";
 		}
 		cout << "\n";
 	}
