1adcc2a68c4409bbd03aea09a3b1a3f23caf1b89
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-02-29 09:33:41 +0900

    [Gold IV] Title: 연구소, Time: 40 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -2,43 +2,21 @@
 
 using namespace std;
 
-const int n = 9;
-const int wall = 3;
-const int dy[] = { -1,0,1,0 };
-const int dx[] = { 0,1,0,-1 };
-
 int N, M;
-int arr[n][n];
-int visited[n][n];
-int cpy[n][n];
-int x, x2, x3, y, y2, y3;
-int SA = INT_MIN;
+int mx = 0;
+int arr[10][10];
+int visited[10][10];
+int tmp[10][10];
 
-vector<pair<int, int>> v;
-vector<vector<int>> cb;
-vector<int> tmp;
+const int dy[] = { -1, 0, 1, 0 };
+const int dx[] = { 0, 1, 0, -1 };
 
-void combi(int s, vector<int> b)
-{
-	if (b.size() == wall)
-	{
-		cb.push_back(b);
-		return;
-	}
+vector<pair<int, int>> walls;
 
-	for (int i = s + 1; i < v.size(); i++)
-	{
-		b.push_back(i);
-		combi(i, b);
-		b.pop_back();
-	}
-	return;
-}
-
-void DFS(int y, int x)
+void dfs(int y, int x)
 {
 	visited[y][x] = 1;
-	cpy[y][x] = 2;
+	tmp[y][x] = 2;
 
 	for (int i = 0; i < 4; i++)
 	{
@@ -46,85 +24,66 @@ void DFS(int y, int x)
 		int nx = x + dx[i];
 
 		if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-		if (visited[ny][nx] == 0 && cpy[ny][nx] == 0)
-		{
-			DFS(ny, nx);
-		}
+		if (visited[ny][nx] == 0 && tmp[ny][nx] == 0)
+			dfs(ny, nx);
 	}
-}
 
-int SafeArea()
-{
-	int ret = 0;
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (cpy[i][j] == 0)
-				ret++;
-		}
-	}
-	return ret;
+	return;
 }
 
 int main(void)
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
 	cin >> N >> M;
+
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < M; j++)
 		{
 			cin >> arr[i][j];
-			if (arr[i][j] == 0)
-			{
-				v.push_back({ i, j });
-			}
+			if (arr[i][j] == 0) walls.push_back({ i, j });
 		}
 	}
-	combi(-1, tmp);
 
-	for (int i = 0; i < cb.size(); i++)
+	for (int i = 0; i < walls.size(); i++)
 	{
-		memcpy(cpy, arr, sizeof(arr));
-		memset(visited, 0, sizeof(visited));
-
-		y = v[cb[i][0]].first;
-		x = v[cb[i][0]].second;
-		
-		y2 = v[cb[i][1]].first;
-		x2 = v[cb[i][1]].second;
-
-		y3 = v[cb[i][2]].first;
-		x3 = v[cb[i][2]].second;
-
-		cpy[y][x] = 1;
-		cpy[y2][x2] = 1;
-		cpy[y3][x3] = 1;
-	
-		for (int a = 0; a < N; a++)
+		for (int j = i + 1; j < walls.size(); j++)
 		{
-			for (int b = 0; b < M; b++)
+			for (int k = j + 1; k < walls.size(); k++)
 			{
-				if (cpy[a][b] == 2 && visited[a][b] == 0)
+				memcpy(tmp, arr, sizeof(arr));
+				memset(visited, 0, sizeof(visited));
+				int cnt = 0;
+
+				tmp[walls[i].first][walls[i].second] = 1;
+				tmp[walls[j].first][walls[j].second] = 1;
+				tmp[walls[k].first][walls[k].second] = 1;
+
+				for (int y = 0; y < N; y++)
 				{
-					DFS(a, b);
+					for (int x = 0; x < M; x++)
+					{
+						if (tmp[y][x] == 2 && visited[y][x] == 0)
+							dfs(y, x);
+					}
 				}
+
+				for (int y = 0; y < N; y++)
+				{
+					for (int x = 0; x < M; x++)
+					{
+						if (tmp[y][x] == 0) cnt++;
+					}
+				}
+
+				mx = max(cnt, mx);
 			}
 		}
-
-		int tmp = SafeArea();
-		/*cout << "-----" << tmp << "--------\n";
-		for (int a = 0; a < N; a++)
-		{
-			for (int b = 0; b < M; b++)
-			{
-				cout << cpy[a][b] << " ";
-			}
-			cout << "\n";
-		}*/
-		SA = max(tmp, SA);
-
 	}
-	cout << SA << "\n";
+	
+	cout << mx << "\n";
+	
 	return 0;
 }
