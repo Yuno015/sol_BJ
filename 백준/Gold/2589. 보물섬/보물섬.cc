2c6983c704d03f26634fa872bfa49e2dac6ed9d5
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-03-04 10:26:10 +0900

    [Gold V] Title: 보물섬, Time: 140 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -2,77 +2,71 @@
 
 using namespace std;
 
-const int n = 54;
-char arr[n][n];
-int visited[n][n];
-int M, N;
-int ret = INT_MIN;
+int N, M;
+int y, x;
+int mx = -1;
+char arr[54][54];
+int visited[54][54];
+vector<pair<int, int>> v;
 
 const int dy[] = { -1, 0, 1, 0 };
 const int dx[] = { 0, 1, 0, -1 };
 
 int main(void)
 {
-	cin >> M >> N;
-	cin.ignore();
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 
-	for (int i = 0; i < M; i++)
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++)
 	{
-		string temp;
-		getline(cin, temp);
-		for (int j = 0; j < temp.size(); j++)
+		string s;
+		cin >> s;
+		for (int j = 0; j < M; j++)
 		{
-			arr[i][j] = temp[j];
+			arr[i][j] = s[j];
+			if (arr[i][j] == 'L') v.push_back({ i, j });
 		}
 	}
 
-	for (int i = 0; i < M; i++)
+	for (int i = 0; i < v.size(); i++)
 	{
-		for (int j = 0; j < N; j++)
+		memset(visited, 0, sizeof(visited));
+		queue<pair<int, int>> q;
+
+		q.push({ v[i].first, v[i].second });
+		visited[v[i].first][v[i].second] = 1;
+		while (!q.empty())
 		{
-			if (arr[i][j] == 'L')
+			tie(y, x) = q.front(); q.pop();
+
+			for (int n = 0; n < 4; n++)
 			{
-				int temp = -1;
-				memset(visited, 0, sizeof(visited));
-				queue<pair<int, int>> q;
+				int ny = y + dy[n];
+				int nx = x + dx[n];
 
-				q.push({ i, j });
-				visited[i][j] = 1;
-
-				while (q.size())
+				if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
+				if (arr[ny][nx] == 'L' && visited[ny][nx] == 0)
 				{
-					int y = q.front().first;
-					int x = q.front().second;
-					q.pop();
-
-					for (int k = 0; k < 4; k++)
-					{
-						int ny = y + dy[k];
-						int nx = x + dx[k];
-
-						if (ny < 0 || nx < 0 || ny >= M || nx >= N) continue;
-						if (arr[ny][nx] == 'L' && visited[ny][nx] == 0)
-						{
-							visited[ny][nx] = visited[y][x] + 1;
-							q.push({ ny, nx });
-						}
-					}
+					visited[ny][nx] = visited[y][x] + 1;
+					q.push({ ny, nx });
 				}
-
-				for (int a = 0; a < M; a++)
-				{
-					for (int b = 0; b < N; b++)
-					{
-						if (visited[a][b] > temp) temp = visited[a][b];
-					}
-				}
-
-				ret = max(ret, temp);
 			}
 		}
+		int tmp = -1;
+
+		for (int a = 0; a < N; a++)
+		{
+			for (int b = 0; b < M; b++)
+			{
+				tmp = max(tmp, visited[a][b] - 1);
+			}
+		}
+
+		mx = max(tmp, mx);
 	}
 
-	cout << ret - 1 << "\n";
-
+	cout << mx << "\n";
 	return 0;
 }
