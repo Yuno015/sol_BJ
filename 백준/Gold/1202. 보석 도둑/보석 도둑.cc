18774eaa178615631d3fa59e23131c656306dd7c
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-03-29 23:35:56 +0900

    [Gold II] Title: 보석 도둑, Time: 516 ms, Memory: 16372 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, k;
+long long sum = 0;
+
+int main(void)
+{
+	cin >> n >> k;
+
+	vector<pair<int, int>> bs;
+	vector<int> bag;
+
+	for (int i = 0; i < n; i++)
+	{
+		int m, v;
+		cin >> m >> v;
+		bs.push_back({ m, v });
+	}
+	for (int i = 0; i < k; i++)
+	{
+		int c;
+		cin >> c;
+		bag.push_back(c);
+	}
+
+	sort(bs.begin(), bs.end());
+	sort(bag.begin(), bag.end());
+	priority_queue<long long> pq;
+
+	int j = 0;
+	for (int i = 0; i < k; i++)
+	{
+		while (j < n && bag[i] >= bs[j].first) pq.push(bs[j++].second);
+		if (pq.size())
+		{
+			sum += pq.top(); pq.pop();
+		}
+	}
+
+	cout << sum << "\n";
+	return 0;
+}
