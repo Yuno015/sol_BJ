941398bbf7be99a180dd64e1eecfd5fca3e22e64
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-07-03 12:10:01 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2392 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -2,37 +2,31 @@
 
 using namespace std;
 
+int N, ret;
+string s;
+
 int main(void)
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
-	int n;
-	cin >> n;
+	cin >> N;
 
-	int ret = 0;
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < N; i++)
 	{
-		string s;
 		cin >> s;
-		if (s.length() % 2 != 0) continue;
-		else
+		stack<char> st;
+		for (int j = 0; j < s.length(); j++)
 		{
-			stack<char> st;
-			for (int j = 0; j < s.length(); j++)
+			if (st.empty()) st.push(s[j]);
+			else
 			{
-				if (st.empty()) st.push(s[j]);
-				else
-				{
-					if (st.top() == s[j]) st.pop();
-					else st.push(s[j]);
-				}
+				if (st.top() == s[j]) st.pop();
+				else st.push(s[j]);
 			}
-			if (st.empty()) ret++;
 		}
+		if (st.empty()) ret++;
 	}
-
-	cout << ret << '\n';
+	cout << ret;
 	return 0;
 }
