dcd12ed4bfccf0fc7f4c724cbcf4ed021b621c65
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-07-23 22:44:01 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -2,34 +2,19 @@
 
 using namespace std;
 
-int arr[54][54];
-int visited[54][54];
+int T, M, N, K, X, Y, cnt;
+int arr[52][52];
+int visited[52][52];
 
-const int dy[] = { -1, 0, 1, 0 };
-const int dx[] = { 0, 1, 0, -1 };
-
-int M, N, K;
-int cnt;
-
-void dfs(int y, int x)
-{
-	visited[y][x] = cnt;
-	for (int i = 0; i < 4; i++)
-	{
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-
-		if (ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
-		if (arr[ny][nx] == 0 || visited[ny][nx] != 0) continue;
-		dfs(ny, nx);
-	}
-}
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
 
 int main(void)
 {
-	int T;
-	cin >> T;
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
+	cin >> T;
 	while (T--)
 	{
 		memset(arr, 0, sizeof(arr));
@@ -40,10 +25,12 @@ int main(void)
 
 		for (int i = 0; i < K; i++)
 		{
-			int x, y;
-			cin >> x >> y;
-			arr[y][x] = 1;
+			cin >> X >> Y;
+
+			arr[Y][X] = 1;
 		}
+
+		stack<pair<int, int>> s;
 
 		for (int i = 0; i < N; i++)
 		{
@@ -52,7 +39,25 @@ int main(void)
 				if (arr[i][j] == 1 && visited[i][j] == 0)
 				{
 					cnt++;
-					dfs(i, j);
+					s.push({ i,j });
+
+					while (!s.empty())
+					{
+						int y = s.top().first;
+						int x = s.top().second;
+						s.pop();
+						visited[y][x] = cnt;
+
+						for (int k = 0; k < 4; k++)
+						{
+							int ny = y + dy[k];
+							int nx = x + dx[k];
+
+							if (ny < 0 || nx < 0 || ny > N || nx > M) continue;
+							if (arr[ny][nx] == 0 || visited[ny][nx] != 0) continue;
+							s.push({ ny, nx });
+						}
+					}
 				}
 			}
 		}
