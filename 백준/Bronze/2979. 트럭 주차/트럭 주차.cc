a21370feba8503131c4e4f5f81dbcdee762c57b7
Yuno <131260594+Yuno015@users.noreply.github.com>
2024-09-16 15:55:53 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -2,34 +2,32 @@
 
 using namespace std;
 
+int arr[105];
+int sum;
+
 int main(void)
 {
 	int A, B, C;
-	int arr[105];
-	int sum = 0;
-
-	memset(arr, 0, sizeof(arr));
-
 	cin >> A >> B >> C;
 
 	for (int i = 0; i < 3; i++)
 	{
-		int in, out;
-		cin >> in >> out;
+		int s, e;
+		cin >> s >> e;
 
-		for (int j = in; j < out; j++)
+		for (int j = s; j < e; j++)
 		{
 			arr[j]++;
 		}
 	}
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 1; i <= 100; i++)
 	{
 		if (arr[i] == 1) sum += A;
-		else if (arr[i] == 2) sum += (2 * B);
-		else if (arr[i] == 3) sum += (3 * C);
+		else if (arr[i] == 2) sum += 2 * B;
+		else if (arr[i] == 3) sum += 3 * C;
 	}
 
-	cout << sum << '\n';
+	cout << sum;
 	return 0;
 }
